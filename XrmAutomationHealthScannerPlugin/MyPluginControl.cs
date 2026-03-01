758faa2bc470149b7a0a5ba65c2e5327a9a83f89
calsora <calumjharrison@gmail.com>
2026-03-01 13:56:01 +0000

    Add project files.

---
+++ XrmAutomationHealthScannerPlugin/MyPluginControl.cs
@@ -0,0 +1,213 @@
+﻿using McTools.Xrm.Connection;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+using XrmToolBox.Extensibility;
+using XrmToolBox.Extensibility.Interfaces;
+
+namespace XrmAutomationHealthScannerPlugin
+{
+    public partial class MyPluginControl : PluginControlBase, IGitHubPlugin, IHelpPlugin
+    {
+        private Settings mySettings;
+
+        public string RepositoryName => "test";
+        public string UserName => "calsora";
+        public string HelpUrl => "http://www.google.com";
+
+        public MyPluginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void MyPluginControl_Load(object sender, EventArgs e)
+        {
+            ShowInfoNotification("This is a notification that can lead to XrmToolBox repository", new Uri("https://github.com/MscrmTools/XrmToolBox"));
+
+            // Loads or creates the settings for the plugin
+            if (!SettingsManager.Instance.TryLoad(GetType(), out mySettings))
+            {
+                mySettings = new Settings();
+
+                LogWarning("Settings not found => a new settings file has been created!");
+            }
+            else
+            {
+                LogInfo("Settings found and loaded");
+            }
+
+            // disable controls
+            SetControlEnabled(false);
+        }
+
+
+        private void SetControlEnabled(bool isEnabled)
+        {
+            groupBox1.Enabled = isEnabled;
+
+            foreach (var cb in new[] { workflowscheckBox, bizrulescheckBox, cloudflowscheckBox })
+            {
+                cb.Enabled = groupBox1.Enabled;
+            }
+
+            nameSearchInput.Enabled = isEnabled;
+        }
+
+
+        private void tsbClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
+
+        // Loads all deactivated workflows (statecode = 1) and binds a simple projection to the grid
+        private void LoadDisabledAutomations()
+        {
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Getting disabled workflows",
+                Work = (worker, args) =>
+                {
+                    var query = new QueryExpression("workflow")
+                    {
+                        ColumnSet = new ColumnSet("name", "createdon", "createdby", "modifiedon", "modifiedby", "category"),
+                        Orders = { new OrderExpression("name", OrderType.Ascending) },
+                        PageInfo = new PagingInfo { Count = 5000, PageNumber = 1 }
+                    };
+
+                    var selectedCategories = new List<int>();
+
+                    foreach (var cb in new[] { workflowscheckBox, bizrulescheckBox, cloudflowscheckBox })
+                    {
+                        if (cb.Checked && cb.Tag != null)
+                        {
+                            if (int.TryParse(cb.Tag.ToString(), out int categoryValue))
+                            {
+                                selectedCategories.Add(categoryValue);
+                            }
+                        }
+                    }
+
+                    query.Criteria.AddCondition("statecode", ConditionOperator.Equal, 0);
+                    query.Criteria.AddCondition("ismanaged", ConditionOperator.Equal, false);
+                    if (selectedCategories.Any())
+                    {
+                        query.Criteria.AddCondition("category", ConditionOperator.In, selectedCategories.Cast<object>().ToArray());
+                    }
+
+                    string nameSearchInputValue = nameSearchInput.Text.Trim().ToLower();
+
+                    if (!string.IsNullOrWhiteSpace(nameSearchInputValue))
+                    {
+                        query.Criteria.AddCondition("name", ConditionOperator.BeginsWith, nameSearchInputValue);
+                    }
+
+
+                    var all = new List<Entity>();
+                    while (true)
+                    {
+                        var page = Service.RetrieveMultiple(query);
+                        all.AddRange(page.Entities);
+                        if (!page.MoreRecords) break;
+                        query.PageInfo.PageNumber++;
+                        query.PageInfo.PagingCookie = page.PagingCookie;
+                    }
+
+                    args.Result = all;
+                },
+                PostWorkCallBack = (args) =>
+                {
+                    if (args.Error != null)
+                    {
+                        MessageBox.Show(args.Error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    var entities = args.Result as List<Entity> ?? new List<Entity>();
+
+                    var rows = entities.Select(x => new
+                    {
+                        Name = x.GetAttributeValue<string>("name"),
+                        CreatedOn = x.GetAttributeValue<DateTime?>("createdon"),
+                        CreatedBy = x.GetAttributeValue<EntityReference>("createdby")?.Name,
+                        ModifiedOn = x.GetAttributeValue<DateTime?>("modifiedon"),
+                        ModifiedBy = x.GetAttributeValue<EntityReference>("modifiedby")?.Name,
+                        Category = x.FormattedValues.Contains("category") ? x.FormattedValues["category"] : x.GetAttributeValue<OptionSetValue>("category")?.Value.ToString(),
+
+                    }).ToList();
+
+                    crmGridView2.AutoGenerateColumns = true;
+                    crmGridView2.DataSource = rows;
+                    crmGridView2.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+                    crmGridView2.Refresh();
+                }
+            });
+
+            SetControlEnabled(true);
+
+        }
+
+        /// <summary>
+        /// This event occurs when the plugin is closed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MyPluginControl_OnCloseTool(object sender, EventArgs e)
+        {
+            // Before leaving, save the settings
+            SettingsManager.Instance.Save(GetType(), mySettings);
+        }
+
+        /// <summary>
+        /// This event occurs when the connection has been updated in XrmToolBox
+        /// </summary>
+        public override void UpdateConnection(IOrganizationService newService, ConnectionDetail detail, string actionName, object parameter)
+        {
+            base.UpdateConnection(newService, detail, actionName, parameter);
+
+            if (mySettings != null && detail != null)
+            {
+                mySettings.LastUsedOrganizationWebappUrl = detail.WebApplicationUrl;
+                LogInfo("Connection has changed to: {0}", detail.WebApplicationUrl);
+            }
+
+            // Ensure the grid has the current service so it can be used if needed by the grid control
+            crmGridView2.OrganizationService = newService;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Ensure connection and then load disabled automations
+            ExecuteMethod(LoadDisabledAutomations);
+        }
+
+        private void textBoxWithPlaceholder1_TextChanged(object sender, EventArgs e)
+        {
+            ExecuteMethod(LoadDisabledAutomations);
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void splitContainer2_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XrmAutomationHealthScannerPlugin/MyPluginControl.designer.cs
@@ -0,0 +1,241 @@
+﻿namespace XrmAutomationHealthScannerPlugin
+{
+    partial class MyPluginControl
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crmGridView2 = new xrmtb.XrmToolBox.Controls.CRMGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.nameSearchInput = new XrmToolBox.Controls.TextBoxWithPlaceholder();
+            this.bizrulescheckBox = new System.Windows.Forms.CheckBox();
+            this.cloudflowscheckBox = new System.Windows.Forms.CheckBox();
+            this.workflowscheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.crmGridView2)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // crmGridView2
+            // 
+            this.crmGridView2.AllowUserToOrderColumns = true;
+            this.crmGridView2.AllowUserToResizeRows = false;
+            this.crmGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.crmGridView2.ColumnOrder = "";
+            this.crmGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crmGridView2.FilterColumns = "";
+            this.crmGridView2.Location = new System.Drawing.Point(0, 0);
+            this.crmGridView2.Name = "crmGridView2";
+            this.crmGridView2.OrganizationService = null;
+            this.crmGridView2.RowHeadersWidth = 51;
+            this.crmGridView2.RowTemplate.Height = 24;
+            this.crmGridView2.Size = new System.Drawing.Size(782, 347);
+            this.crmGridView2.TabIndex = 1;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(782, 31);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(107, 28);
+            this.toolStripButton1.Text = "Close this tool";
+            this.toolStripButton1.Click += new System.EventHandler(this.tsbClose_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.toolStripButton2.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(186, 28);
+            this.toolStripButton2.Text = "Scan inactive automations";
+            this.toolStripButton2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.AccessibleDescription = "hello";
+            this.splitContainer2.Panel1.AccessibleName = "Helllo";
+            this.splitContainer2.Panel1.AllowDrop = true;
+            this.splitContainer2.Panel1.Controls.Add(this.toolStrip1);
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer2.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel1_Paint);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.crmGridView2);
+            this.splitContainer2.Size = new System.Drawing.Size(786, 475);
+            this.splitContainer2.SplitterDistance = 120;
+            this.splitContainer2.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.nameSearchInput);
+            this.groupBox1.Controls.Add(this.bizrulescheckBox);
+            this.groupBox1.Controls.Add(this.cloudflowscheckBox);
+            this.groupBox1.Controls.Add(this.workflowscheckBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 43);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(582, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter options";
+            // 
+            // nameSearchInput
+            // 
+            this.nameSearchInput.Enabled = false;
+            this.nameSearchInput.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.nameSearchInput.Location = new System.Drawing.Point(349, 39);
+            this.nameSearchInput.Name = "nameSearchInput";
+            this.nameSearchInput.Placeholder = null;
+            this.nameSearchInput.Size = new System.Drawing.Size(152, 22);
+            this.nameSearchInput.TabIndex = 1;
+            this.nameSearchInput.Tag = true;
+            this.nameSearchInput.TextChanged += new System.EventHandler(this.textBoxWithPlaceholder1_TextChanged);
+            // 
+            // bizrulescheckBox
+            // 
+            this.bizrulescheckBox.AutoSize = true;
+            this.bizrulescheckBox.Checked = true;
+            this.bizrulescheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.bizrulescheckBox.Enabled = false;
+            this.bizrulescheckBox.Location = new System.Drawing.Point(6, 39);
+            this.bizrulescheckBox.Name = "bizrulescheckBox";
+            this.bizrulescheckBox.Size = new System.Drawing.Size(116, 20);
+            this.bizrulescheckBox.TabIndex = 3;
+            this.bizrulescheckBox.Tag = "2";
+            this.bizrulescheckBox.Text = "Business rules";
+            this.bizrulescheckBox.UseVisualStyleBackColor = true;
+            this.bizrulescheckBox.CheckedChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // cloudflowscheckBox
+            // 
+            this.cloudflowscheckBox.AutoSize = true;
+            this.cloudflowscheckBox.Checked = true;
+            this.cloudflowscheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cloudflowscheckBox.Enabled = false;
+            this.cloudflowscheckBox.Location = new System.Drawing.Point(139, 39);
+            this.cloudflowscheckBox.Name = "cloudflowscheckBox";
+            this.cloudflowscheckBox.Size = new System.Drawing.Size(97, 20);
+            this.cloudflowscheckBox.TabIndex = 2;
+            this.cloudflowscheckBox.Tag = "5";
+            this.cloudflowscheckBox.Text = "Cloud flows";
+            this.cloudflowscheckBox.UseVisualStyleBackColor = true;
+            this.cloudflowscheckBox.CheckedChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // workflowscheckBox
+            // 
+            this.workflowscheckBox.AutoSize = true;
+            this.workflowscheckBox.Checked = true;
+            this.workflowscheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.workflowscheckBox.Enabled = false;
+            this.workflowscheckBox.Location = new System.Drawing.Point(242, 39);
+            this.workflowscheckBox.Name = "workflowscheckBox";
+            this.workflowscheckBox.Size = new System.Drawing.Size(91, 20);
+            this.workflowscheckBox.TabIndex = 0;
+            this.workflowscheckBox.Tag = "0";
+            this.workflowscheckBox.Text = "Workflows";
+            this.workflowscheckBox.UseVisualStyleBackColor = true;
+            this.workflowscheckBox.CheckedChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // MyPluginControl
+            // 
+            this.Controls.Add(this.splitContainer2);
+            this.Name = "MyPluginControl";
+            this.Size = new System.Drawing.Size(786, 475);
+            ((System.ComponentModel.ISupportInitialize)(this.crmGridView2)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStripMenu;
+        private System.Windows.Forms.ToolStripButton tsbClose;
+        private System.Windows.Forms.ToolStripButton tsbSample;
+        private System.Windows.Forms.ToolStripSeparator tssSeparator1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private xrmtb.XrmToolBox.Controls.EntitiesDropdownControl entitiesDropdownControl1;
+        private xrmtb.XrmToolBox.Controls.Controls.CDSDataComboBox cdsDataComboBox1;
+        private xrmtb.XrmToolBox.Controls.CRMGridView crmGridView1;
+        private xrmtb.XrmToolBox.Controls.CRMGridView crmGridView2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox cloudflowscheckBox;
+        private System.Windows.Forms.CheckBox workflowscheckBox;
+        private System.Windows.Forms.CheckBox bizrulescheckBox;
+        private XrmToolBox.Controls.TextBoxWithPlaceholder nameSearchInput;
+    }
+}
